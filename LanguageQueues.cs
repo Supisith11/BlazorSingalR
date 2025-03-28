ee1288f1a7f03f93dab27dc7ad9bc622214310a0
Supriya <supisith11@gmail.com>
2025-03-28 04:56:00 -0500

    Add files via upload

---
+++ LanguageQueues.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorSignalRApp.Client
+{
+    public class LanguageQueues
+    {
+        public List<string> VolunteerQueue { get; set; } = new List<string>();
+        public List<string> HelpQueue { get; set; } = new List<string>();
+    }
+}

e4ee4f997d08a5487a219449bd94c05c226818a1
Supriya <supisith11@gmail.com>
2025-04-07 21:53:10 -0500

    Add files via upload

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatApp2.Hubs;
+
+public class ChatHub : Hub
+{
+    public async Task JoinRoom(string roomName, string userName)
+    {
+        await Groups.AddToGroupAsync(Context.ConnectionId, roomName);
+        // Notify the client that they've successfully joined the room
+        //await Clients.Caller.SendAsync("ReceiveMessage", "System", $"You have joined the room: {roomName}");
+        await Clients.Group(roomName).SendAsync("ReceiveMessage", "System", $"{userName} has joined the room.");
+    }
+
+    public async Task LeaveRoom(string roomName)
+    {
+        await Groups.RemoveFromGroupAsync(Context.ConnectionId, roomName);
+    }
+
+    public async Task SendMessage(string user, string message, string roomName)
+    {
+        await Clients.Group(roomName).SendAsync("ReceiveMessage", user, message);
+    }
+}

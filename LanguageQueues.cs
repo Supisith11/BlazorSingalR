14637e292355ed9ca738b1273a254ab978a242ca
Supriya <supisith11@gmail.com>
2025-04-07 21:57:35 -0500

    Delete LanguageQueues.cs

--- LanguageQueues.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BlazorSignalRApp.Client
-{
-    public class LanguageQueues
-    {
-        public List<string> VolunteerQueue { get; set; } = new List<string>();
-        public List<string> HelpQueue { get; set; } = new List<string>();
-    }
-}
